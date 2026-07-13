f4033bf017f410849780ec50260fbcc624ec2154
Josh986 <8155218+Josh986@users.noreply.github.com>
2026-07-13 07:38:35 +0000

    feat: add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YOUR-DOMAIN-HERE.com',
+  site: 'https://josh986.github.io',
+  base: '/silvia-ly-realtor',
   output: 'static',
   integrations: [tailwind()],
 });
